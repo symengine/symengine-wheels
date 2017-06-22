46ad767e5fad176b829c389ebf9ec311b16bb554
Isuru Fernando <isuruf@gmail.com>
2017-06-22 20:51:31 +0530

    Fix windows wheel

---
+++ fix_windows_wheel.py
@@ -0,0 +1,23 @@
+import sys
+from os.path import join as pjoin
+import shutil
+from glob import glob
+
+from delocate import wheeltools
+
+def add_library(lib_paths, dist_path='dist'):
+    wheel_fnames = glob(pjoin(dist_path, '*.whl'))
+    for fname in wheel_fnames:
+        print('Processing', fname)
+        with wheeltools.InWheel(fname, fname):
+            for lib_path in lib_paths:
+                shutil.copy2(lib_path, pjoin('symengine', 'lib'))
+
+
+def main():
+    args = list(sys.argv)
+    args.pop(0)
+    add_library(args)
+
+if __name__ == '__main__':
+    main()
