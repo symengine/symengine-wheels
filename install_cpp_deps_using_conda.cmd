6318481fdc58b5960ee4888b0dba920b2fe01d11
Isuru Fernando <isuruf@gmail.com>
2017-06-22 20:38:39 +0530

    Install test deps

--- install_cpp_deps_using_conda.cmd
+++ install_cpp_deps_using_conda.cmd
@@ -5,7 +5,6 @@ call %CONDA_INSTALL_LOCN%\Scripts\activate.bat
 conda config --add channels conda-forge
 conda create -n symengine --yes symengine=0.3.0 mpir=3.0.0 vc=14
 call activate symengine
-conda remove --yes --force vc
 conda remove --yes --force vs2015_runtime
 
 ::back to Standard commission
