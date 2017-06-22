fafcf5aecdfd67415d8e6903df17e71ef8b4cd5d
Isuru Fernando <isuruf@gmail.com>
2017-06-22 20:19:55 +0530

    Add --yes

--- install_cpp_deps_using_conda.cmd
+++ install_cpp_deps_using_conda.cmd
@@ -3,10 +3,10 @@ SETLOCAL
 
 call %CONDA_INSTALL_LOCN%\Scripts\activate.bat
 conda config --add channels conda-forge
-conda create -n symengine symengine=0.3.0 mpir=3.0.0 vc=14
+conda create -n symengine --yes symengine=0.3.0 mpir=3.0.0 vc=14
 activate symengine
-conda remove --force vc
-conda remove --force vs2015_runtime
+conda remove --yes --force vc
+conda remove --yes --force vs2015_runtime
 
 ::back to Standard commission
 ENDLOCAL
