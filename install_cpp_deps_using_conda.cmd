13ebd3b6081d80ad51720bc7377f79fcfdb8120b
Isuru Fernando <isuruf@gmail.com>
2017-06-23 08:54:56 +0530

    Upload mpir in appveyor

--- install_cpp_deps_using_conda.cmd
+++ install_cpp_deps_using_conda.cmd
@@ -6,6 +6,8 @@ conda config --add channels conda-forge
 conda create -n symengine --yes symengine=0.3.0 mpir=3.0.0 vc=14
 call activate symengine
 conda remove --yes --force vs2015_runtime
+mkdir symengine/dist
+if [%UPLOAD_ARCHIVES%] == [yes] appveyor DownloadFile http://mpir.org/mpir-3.0.0.tar.bz2 && mv mpir-3.0.0.tar.bz2 symengine/dist/mpir-3.0.0.tar.bz2
 
 ::back to Standard commission
 ENDLOCAL
