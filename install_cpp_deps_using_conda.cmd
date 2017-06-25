1d72cc6f0d33e5a9b6accef410ac06225e8cc290
Isuru Fernando <isuruf@gmail.com>
2017-06-25 14:59:06 +0530

    Use mpfr and mpc on windows

--- install_cpp_deps_using_conda.cmd
+++ install_cpp_deps_using_conda.cmd
@@ -3,10 +3,13 @@ SETLOCAL
 
 mkdir symengine\dist
 if [%UPLOAD_ARCHIVES%] == [yes] appveyor DownloadFile http://mpir.org/mpir-3.0.0.tar.bz2 && mv mpir-3.0.0.tar.bz2 symengine/dist/mpir-3.0.0.tar.bz2
+if [%UPLOAD_ARCHIVES%] == [yes] appveyor DownloadFile https://github.com/isuruf/mpfr/archive/3.1.5-win.tar.gz && mv 3.1.5-win.tar.gz symengine/dist/mpfr-3.1.5-win.tar.gz
+if [%UPLOAD_ARCHIVES%] == [yes] appveyor DownloadFile https://github.com/isuruf/mpc/archive/1.0.3-win.tar.gz && mv 1.0.3-win.tar.gz symengine/dist/mpc-1.0.3-win.tar.gz
+
 
 call %CONDA_INSTALL_LOCN%\Scripts\activate.bat
 conda config --add channels conda-forge
-conda create -n symengine --yes symengine=0.3.0 mpir=3.0.0 vc=14
+conda create -n symengine --yes symengine=0.3.0 mpir=3.0.0 mpfr=3.1.5 mpc=1.0.3 vc=14
 call activate symengine
 conda remove --yes --force vs2015_runtime
 
