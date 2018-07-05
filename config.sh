3d3ed11583ebc58633584272296575d46bd172b8
Isuru Fernando <isuruf@gmail.com>
2018-07-04 19:47:56 -0600

    deactivate conda

--- config.sh
+++ config.sh
@@ -42,6 +42,7 @@ function install_llvm {
         conda config --set show_channel_urls true
         conda create -y -q -p `pwd`/llvm llvmdev=5.0.0
         sudo rsync -av `pwd`/llvm/ $BUILD_PREFIX/
+        source /Users/travis/miniconda3/bin/deactivate
     else
         yum install xz -y
         mkdir llvm-5.0.1 && cd llvm-5.0.1
