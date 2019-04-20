709f8ee0ac687b9e8e4c0383b9a309d3c88a165a
Isuru Fernando <isuruf@gmail.com>
2019-04-20 13:52:02 -0500

    Fix issue with path separators

--- fix_windows_wheel.py
+++ fix_windows_wheel.py
@@ -1,7 +1,10 @@
 import sys
 from os.path import join as pjoin
+import os
 import shutil
 from glob import glob
+import tempfile
+import subprocess
 
 from delocate import wheeltools
 
@@ -9,10 +12,16 @@ def add_library(lib_paths, dist_path='dist'):
     wheel_fnames = glob(pjoin(dist_path, '*.whl'))
     for fname in wheel_fnames:
         print('Processing', fname)
+        fname = os.path.abspath(fname)
         with wheeltools.InWheel(fname, fname):
             for lib_path in lib_paths:
                 shutil.copy2(lib_path, pjoin('symengine', 'lib'))
 
+        dirpath = tempfile.mkdtemp()
+        os.chdir(dirpath)
+        subprocess.check_call(["7z", "x", fname])
+        subprocess.check_call(["7z", "a", fname+".zip"])
+        subprocess.check_call(["mv", fname+".zip", fname])
 
 def main():
     args = list(sys.argv)
