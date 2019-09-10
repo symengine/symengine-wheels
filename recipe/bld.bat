0f81dcf564adec564f479a1c315e8e73a3be8db2
Isuru Fernando <isuruf@gmail.com>
2019-09-10 17:19:36 -0500

    Fix windows command

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,1 +1,1 @@
-python setup.py install --symengine-dir=%LIBRARY_PREFIX% --generator=Ninja --single-version-externally-managed --record record.txt
+python setup.py build_ext --generator=Ninja install --symengine-dir=%LIBRARY_PREFIX% --single-version-externally-managed --record record.txt
