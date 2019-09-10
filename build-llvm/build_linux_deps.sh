a9714d3c4560f7446353a4311804d7c07a8216ba
Isuru Fernando <isuruf@gmail.com>
2019-09-10 02:38:55 -0500

    No need of clang

--- build-llvm/build_linux_deps.sh
+++ build-llvm/build_linux_deps.sh
@@ -16,9 +16,9 @@ yum install -y git yum libxml2-devel xz
 export PATH="/opt/python/cp37-cp37m/lib/python3.7/site-packages/cmake/data/bin/:${PATH}"
 
 # LLVM for pocl
-#curl -L -O https://github.com/llvm/llvm-project/releases/download/llvmorg-${LLVM_VERSION}/llvm-${LLVM_VERSION}.src.tar.xz
-#unxz llvm-${LLVM_VERSION}.src.tar.xz
-#tar -xf llvm-${LLVM_VERSION}.src.tar
+curl -L -O https://github.com/llvm/llvm-project/releases/download/llvmorg-${LLVM_VERSION}/llvm-${LLVM_VERSION}.src.tar.xz
+unxz llvm-${LLVM_VERSION}.src.tar.xz
+tar -xf llvm-${LLVM_VERSION}.src.tar
 pushd llvm-${LLVM_VERSION}.src
 mkdir -p build
 pushd build
@@ -40,42 +40,16 @@ cmake -DPYTHON_EXECUTABLE=/opt/python/cp37-cp37m/bin/python \
 
 make
 make install
-make install DESTDIR=/install/llvm-$LLVM_VERSION
+make install DESTDIR=/install
 popd
 cp LICENSE.TXT $LICENSE_DIR/LLVM_LICENSE.txt
 popd
 
-# clang for pocl
-#curl -L -O https://github.com/llvm/llvm-project/releases/download/llvmorg-${LLVM_VERSION}/cfe-${LLVM_VERSION}.src.tar.xz
-#unxz cfe-${LLVM_VERSION}.src.tar.xz
-#tar -xf cfe-${LLVM_VERSION}.src.tar
-pushd cfe-${LLVM_VERSION}.src
-mkdir -p build
-pushd build
-cmake \
-    -DCMAKE_INSTALL_PREFIX=/usr/local \
-    -DCMAKE_PREFIX_PATH=/usr/local \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DLLVM_ENABLE_RTTI=ON \
-    -DCLANG_INCLUDE_TESTS=OFF \
-    -DCLANG_INCLUDE_DOCS=OFF \
-    -DLLVM_INCLUDE_TESTS=OFF \
-    -DLLVM_INCLUDE_DOCS=OFF \
-    -DLLVM_ENABLE_LIBXML2=OFF \
-    -DLLVM_ENABLE_ZLIB=OFF \
-    -DLLVM_TEMPORARILY_ALLOW_OLD_TOOLCHAIN=ON \
-    ..
-make
-make install
-make install DESTDIR=/install/llvm-$VERSION
-popd
-cp LICENSE.TXT $LICENSE_DIR/clang_LICENSE.txt
-popd
-
-pushd /install/llvm-$VERSION
+pushd /install/
 mv usr/local/* .
 rm -rf usr
 popd
 
-pushd /io
-tar -czvf llvm-$LLVM_VERSION-manylinux1_x86-64.tar.gz /install/llvm-$VERSION/pocl-$LLVM_VERSION
+pushd /install
+tar -czvf llvm-$LLVM_VERSION-manylinux1_x86-64.tar.gz llvm-$LLVM_VERSION
+mv llvm-$LLVM_VERSION-manylinux1_x86-64.tar.gz /io/
