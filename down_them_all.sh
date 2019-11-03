0e5a12a1f284ed8553f2382f8ec1d5195b7e3c7c
Isuru Fernando <isuruf@gmail.com>
2019-11-02 23:19:59 -0500

    Update down_them_all script

--- down_them_all.sh
+++ down_them_all.sh
@@ -13,18 +13,19 @@ fi
 
 declare -a arr=(
     "-cp27-cp27m-macosx_10_9_x86_64.macosx_10_10_x86_64.whl"
-	"-cp34-cp34m-macosx_10_9_x86_64.macosx_10_10_x86_64.whl"
 	"-cp36-cp36m-macosx_10_9_x86_64.macosx_10_10_x86_64.whl"
 	"-cp37-cp37m-macosx_10_9_x86_64.macosx_10_10_x86_64.whl"
+	"-cp38-cp38-macosx_10_9_x86_64.whl"
 	"-cp27-cp27m-manylinux1_x86_64.whl"
 	"-cp27-cp27mu-manylinux1_x86_64.whl"
-	"-cp34-cp34m-manylinux1_x86_64.whl"
 	"-cp35-cp35m-manylinux1_x86_64.whl"
 	"-cp36-cp36m-manylinux1_x86_64.whl"
 	"-cp37-cp37m-manylinux1_x86_64.whl"
+	"-cp38-cp38-manylinux1_x86_64.whl"
 	"-cp35-cp35m-win_amd64.whl"
 	"-cp36-cp36m-win_amd64.whl"
 	"-cp37-cp37m-win_amd64.whl"
+	"-cp38-cp38-win_amd64.whl"
 	".tar.gz")
 for whl_file in "${arr[@]}"
 do
