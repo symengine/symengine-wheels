ef02778536b7126b1b83352048883e8f905485ee
Isuru Fernando <isuruf@gmail.com>
2021-03-11 00:10:49 -0600

    wheels

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,41 @@
 #!/usr/bin/env bash
 
-source activate "${CONDA_DEFAULT_ENV}"
+pushd cxx
 
-python setup.py install --symengine-dir=$PREFIX --single-version-externally-managed --record record.txt
+mkdir build
+cd build
+
+if [[ "$target_platform" == linux-* ]]; then
+    export LDFLAGS="$LDFLAGS -static-libstdc++"
+fi
+
+cmake \
+    -DCMAKE_PREFIX_PATH=$PREFIX \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_INSTALL_LIBDIR=lib \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_BENCHMARKS=no \
+    -DINTEGER_CLASS=flint \
+    -DWITH_SYMENGINE_THREAD_SAFE=yes \
+    -DWITH_MPC=yes \
+    -DWITH_FLINT=yes \
+    -DWITH_LLVM=yes \
+    -DBUILD_FOR_DISTRIBUTION=yes \
+    -DBUILD_SHARED_LIBS=yes \
+    ..
+
+cmake --build . -- -j${CPU_COUNT}
+cmake --build . --target install
+
+ctest
+
+popd
+
+pushd python
+  python setup.py build_ext -i --symengine-dir=$PREFIX bdist_wheel
+  if [[ "$target_platform" == linux-* ]]; then
+    auditwheel repair dist/*.whl -w $PWD/fixed_wheels
+  else
+    delocate-wheel -w fixed_wheels -v dist/*.whl
+  fi
+popd
