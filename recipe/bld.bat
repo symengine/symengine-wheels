c913437025c1619e627cddfad993fca22de703f7
Isuru Fernando <isuruf@gmail.com>
2021-03-11 20:17:50 -0600

    build script for windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,1 +1,39 @@
-python setup.py build_ext --generator=Ninja install --symengine-dir=%LIBRARY_PREFIX% --single-version-externally-managed --record record.txt
+cd cxx
+
+mkdir build
+cd build
+
+cmake ^
+    -G "Ninja" ^
+    -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DBUILD_BENCHMARKS=no ^
+    -DINTEGER_CLASS=flint ^
+    -DWITH_MPC=yes ^
+    -DWITH_LLVM=yes ^
+    -DWITH_COTIRE=no ^
+    -DWITH_SYMENGINE_THREAD_SAFE=yes ^
+    -DBUILD_FOR_DISTRIBUTION=yes ^
+    -DBUILD_SHARED_LIBS=yes ^
+    -DMSVC_USE_MT=no ^
+    -DBUILD_SHARED_LIBS=no ^
+    ..
+
+ninja -j%CPU_COUNT%
+ninja install
+ctest
+
+cd ..
+cd ..
+
+cd python
+%PYTHON% setup.py bdist_wheel build_ext --generator=Ninja install --symengine-dir=%LIBRARY_PREFIX%
+
+mkdir %SRC_DIR%\build_artifacts
+mkdir %SRC_DIR%\build_artifacts\pypi_wheels
+
+for %%WHL in (dist/*.whl) do (
+  cp %WHL% %SRC_DIR%\build_artifacts\pypi_wheels\
+  %PYTHON% -m pip install %WHL%
+)
