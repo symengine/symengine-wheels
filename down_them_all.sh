d972b69514a837de238de28d4f77a8a7378993a1
Isuru Fernando <isuruf@gmail.com>
2021-11-16 17:58:52 -0600

    add cp310 wheels

--- down_them_all.sh
+++ down_them_all.sh
@@ -4,6 +4,7 @@ mkdir -p dist
 cd dist
 
 version=$1
+version_post=$2
 
 if [[ "$1" == "" ]]; then
     echo "Usage: down_them_all.sh <version>"
@@ -15,24 +16,30 @@ declare -a arr=(
 	"-cp37-cp37m-macosx_10_9_x86_64.whl"
 	"-cp38-cp38-macosx_10_9_x86_64.whl"
 	"-cp39-cp39-macosx_10_9_x86_64.whl"
+	"-cp310-cp310-macosx_10_9_x86_64.whl"
 	"-cp38-cp38-macosx_11_0_arm64.whl"
 	"-cp39-cp39-macosx_11_0_arm64.whl"
+	"-cp310-cp310-macosx_11_0_arm64.whl"
 	"-cp36-cp36m-manylinux2010_x86_64.whl"
 	"-cp37-cp37m-manylinux2010_x86_64.whl"
 	"-cp38-cp38-manylinux2010_x86_64.whl"
 	"-cp39-cp39-manylinux2010_x86_64.whl"
+	"-cp310-cp310-manylinux2010_x86_64.whl"
 	"-cp36-cp36m-manylinux2014_aarch64.whl"
 	"-cp37-cp37m-manylinux2014_aarch64.whl"
 	"-cp38-cp38-manylinux2014_aarch64.whl"
 	"-cp39-cp39-manylinux2014_aarch64.whl"
+	"-cp310-cp310-manylinux2014_aarch64.whl"
 	"-cp36-cp36m-manylinux2014_ppc64le.whl"
 	"-cp37-cp37m-manylinux2014_ppc64le.whl"
 	"-cp38-cp38-manylinux2014_ppc64le.whl"
 	"-cp39-cp39-manylinux2014_ppc64le.whl"
+	"-cp310-cp310-manylinux2014_ppc64le.whl"
 	"-cp36-cp36m-win_amd64.whl"
 	"-cp37-cp37m-win_amd64.whl"
 	"-cp38-cp38-win_amd64.whl"
 	"-cp39-cp39-win_amd64.whl"
+	"-cp310-cp310-win_amd64.whl"
     "-pp36-pypy36_pp73-macosx_10_9_x86_64.whl"
     "-pp37-pypy37_pp73-macosx_10_9_x86_64.whl"
     "-pp36-pypy36_pp73-manylinux2010_x86_64.whl"
@@ -40,7 +47,7 @@ declare -a arr=(
 	".tar.gz")
 for whl_file in "${arr[@]}"
 do
-    curl -L -O https://github.com/symengine/symengine-wheels/releases/download/v${version}/symengine-${version}${whl_file};
+    curl -L -O https://github.com/symengine/symengine-wheels/releases/download/v${version}${version_post}/symengine-${version}${whl_file};
 done
 
 
